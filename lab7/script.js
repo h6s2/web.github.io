49b24a186cdd028469ed6e4ed60225aa60543ec8
h6s2 <h6s2x37kl11s@gmail.com>
2023-01-08 15:32:34 +0200

    lab7

---
+++ lab7/script.js
@@ -0,0 +1,109 @@
+const elem = document.getElementById("circle");
+let pixels=20;   //pixel shift
+
+let x=pixels;
+let y=0;
+let time=400;
+let pause=0;
+let dist1,dist2,dist3,dist4;
+
+document.getElementById("circle").style.transitionDuration=`${(time*0.9)}ms`;
+
+function play(){
+  document.getElementById("work").style.display="flex";
+  document.getElementById("start-button").style.display="inline-block";
+  document.getElementById("close-button").style.display="inline-block";
+}
+
+function start(){
+  document.getElementById("start-button").style.display="none";
+  document.getElementById("stop-button").style.display="inline-block";
+  pause=0;
+  dist1=document.getElementById("anim").getBoundingClientRect().top;
+  dist2=document.getElementById("anim").getBoundingClientRect().bottom;
+  dist3=document.getElementById("anim").getBoundingClientRect().left;
+  dist4=document.getElementById("anim").getBoundingClientRect().right;
+  left();
+}
+function left(){
+  if (pause==1){return;}
+  if (checkBorders("up")){return};
+  elem.style.transform = `translate(${-x}px,${-y}px)`;
+  y+=pixels;
+  setTimeout(down,time);
+}
+function down(){
+  if (pause==1){return;}
+  if (checkBorders("left")){return};
+  elem.style.transform = `translate(${-x}px,${y}px)`;
+  x+=pixels;
+  setTimeout(right,time);
+}
+function right(){
+  if (pause==1){return;}
+  if (checkBorders("down")){return};
+  elem.style.transform = `translate(${x}px,${y}px)`;
+  y+=pixels;
+  setTimeout(up,time);
+}
+function up(){
+  if (pause==1){return;}
+  if (checkBorders("right")){return};
+  elem.style.transform = `translate(${x}px,${-y}px)`;
+  x+=pixels;
+  setTimeout(left,time);
+}
+
+function checkBorders(direction){
+  let d;
+  switch(direction){
+    case "left":
+      d=elem.getBoundingClientRect().left-dist3; break;
+    case "right":
+      d=dist4-elem.getBoundingClientRect().right; break;
+    case "up":
+      d=elem.getBoundingClientRect().top-dist1; break;
+    case "down":
+      d=dist2-elem.getBoundingClientRect().bottom; break;
+  }
+  console.log(d);
+  if(d<-32){
+    document.getElementById("message").innerHTML="Outside the border";
+    setTimeout(reset,1000);
+    return true;
+  }
+  if (d<=0){
+    document.getElementById("message").innerHTML="Border!";
+    document.getElementById("stop-button").style.display="none";
+    document.getElementById("start-button").style.display="none";
+    document.getElementById("reload-button").style.display="inline-block";
+  }
+  return false;
+}
+
+function reset(){
+  pause=1;
+  document.getElementById("stop-button").style.display="none";
+  document.getElementById("reload-button").style.display="none";
+  document.getElementById("start-button").style.display="inline-block";
+  elem.style.transform = `translate(0px,0px)`;
+  x=pixels;
+  y=0;
+}
+
+function stop_anim(){
+  pause=1;
+  document.getElementById("reload-button").style.display="none";
+  document.getElementById("start-button").style.display="inline-block";
+  document.getElementById("stop-button").style.display="none";
+}
+
+function close_window(){
+  pause=1;
+  document.getElementById("work").style.display="none";
+  document.getElementById("start-button").style.display="none";
+  document.getElementById("stop-button").style.display="none";
+  document.getElementById("close-button").style.display="none";
+  document.getElementById("message").innerHTML="";
+  reset();
+}
---
+++ lab7/script2.js
@@ -0,0 +1,122 @@
+let canvas,height,width,ctx,x,y;
+let pixels = 10;
+let shift=1;
+let direction=1;
+let dist=0;
+let id;
+
+function start(){
+  document.getElementById("start-button").style.display="none";
+  document.getElementById("stop-button").style.display="inline-block";
+  document.getElementById("message").innerHTML="";
+  pause=0;
+  id=requestAnimationFrame(move);
+}
+
+function move(){
+  id=requestAnimationFrame(move);
+  if(direction==1){x-=shift;}
+  if(direction==2){y+=shift;}
+  if(direction==3){x+=shift;}
+  if(direction==4){y-=shift;}
+
+  ctx.clearRect(0,0,width,height);
+  ctx.beginPath();
+  ctx.arc(x, y, 16, 0, 2 * Math.PI);
+  ctx.fillStyle="red";
+  ctx.fill();
+  ctx.stroke();
+
+  dist+=1;
+  if (dist==pixels){
+    dist=0;
+    direction+=1;
+    pixels+=1;
+    shift+=0.2;
+    if (direction==5){direction=1;};
+    checkBorders();
+  }
+}
+
+function checkBorders(){
+  if (x<=16||y<=16||x>=width-16||y>=height-16){
+    document.getElementById("message").innerHTML="Border!";
+    document.getElementById("stop-button").style.display="none";
+    document.getElementById("start-button").style.display="none";
+    document.getElementById("reload-button").style.display="inline-block";
+  }
+  if (x<=-16||y<=-16||x>=width+16||y>=height+16){
+    document.getElementById("message").innerHTML="Outside the border";
+    reset();
+  }
+};
+
+function reset(){
+  document.getElementById("stop-button").style.display="none";
+  document.getElementById("reload-button").style.display="none";
+  document.getElementById("start-button").style.display="inline-block";
+  x=width/2;
+  y=height/2;
+  ctx.clearRect(0,0,width,height);
+  ctx.beginPath();
+  ctx.arc(x, y, 16, 0, 2 * Math.PI);
+  ctx.fillStyle="red";
+  ctx.fill();
+  ctx.stroke();
+  shift=1;
+  direction=1;
+  pixels=10;
+  dist=0;
+  cancelAnimationFrame(id);
+}
+
+function play(){
+  if (ctx){return;};
+  document.getElementById("work").style.display="flex";
+  document.getElementById("start-button").style.display="inline-block";
+  document.getElementById("close-button").style.display="inline-block";
+  canvas=document.getElementById("canvas");
+  height=canvas.getBoundingClientRect().height;
+  width=canvas.getBoundingClientRect().width;
+  canvas.width = width;
+  canvas.height = height;
+  ctx = canvas.getContext("2d");
+  x=width/2;
+  y=height/2;
+  ctx.beginPath();
+  ctx.arc(x, y, 16, 0, 2 * Math.PI);
+  ctx.fillStyle="red";
+  ctx.fill();
+  ctx.stroke();
+};
+
+function close_window(){
+  reset();
+  document.getElementById("work").style.display="none";
+  document.getElementById("start-button").style.display="none";
+  document.getElementById("stop-button").style.display="none";
+  document.getElementById("close-button").style.display="none";
+  document.getElementById("message").innerHTML="";
+  ctx=null;
+}
+function stop_anim(){
+  document.getElementById("reload-button").style.display="none";
+  document.getElementById("start-button").style.display="inline-block";
+  document.getElementById("stop-button").style.display="none";
+  cancelAnimationFrame(id);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
